56ace2ff12f0b295fae83cb29a79eae960d8ed0e
melius <melius@rc-macbook-pro.local>
2020-03-13 18:43:49 +0900

    add event listeners

--- src/public/js/maps.js
+++ src/public/js/maps.js
@@ -5,47 +5,70 @@ import { province, seoul, gyeonggi } from './geoJsonList';
 const clog = console.log;
 clog('masps start');
 
-let rgeo = province;
-
+let svg, projection, path;
 let width = 500;
 let height = 500;
 
-let svg = d3.select('#maps').append('svg')
-    .attr("width", width)
-    .attr("height", height);
+showMap(province);
 
-// https://github.com/d3/d3-geo
-// let projection = d3.geoAlbersUsa();
-let projection = d3.geoMercator().center([rgeo.lon, rgeo.lat])
-    .scale(rgeo.scale).rotate([0, 0])
-    .translate([width / 2, height / 2]);
+function showMap(rgeo) {
+    svg = d3.select('#maps').append('svg')
+        .attr("width", width)
+        .attr("height", height);
 
-let path = d3.geoPath().projection(projection);
+    projection = d3.geoMercator().center([rgeo.lon, rgeo.lat])
+        .scale(rgeo.scale).rotate([0, 0])
+        .translate([width / 2, height / 2]);
 
-d3.json(rgeo.url).then(json => {
-    // clog(json);
+    // https://github.com/d3/d3-geo
+    // let projection = d3.geoAlbersUsa();
+    path = d3.geoPath().projection(projection);
 
-    svg.selectAll("path")
-        .data(json.features)
-        .enter().append("path")
-        .attr("d", path)
-        .on("click", (d, index) => {
-            svg.selectAll("path")
-                .classed("selected", d2 => d == d2);
+    d3.json(rgeo.url).then(json => {
+        // clog(json);
+        svg.selectAll("path")
+            .data(json.features)
+            .enter().append("path")
+            .attr("d", path)
+            .on("click", function (d, index) {
+                if ($(this).hasClass("selected")) {
+                    if (d.properties[rgeo.prop_num] == 11) { // seoul
+                        reset();
+                        showMap(seoul);
+                    } else if (d.properties[rgeo.prop_num] == 41) { // gyeonggi
+                        reset();
+                        showMap(gyeonggi);
+                    }
+                }
+                svg.selectAll("path").classed("selected", d2 => d == d2);
+                // svg.selectAll("text").classed("selected", d2 => d == d2);
+            })
+            // .on("mouseenter", () => clog("enter"))
+            // .on("mouseleave", () => clog("leave"));
+            .on("mouseover", d => $("#rname").show())
+            .on("mouseout", d => $("#rname").hide())
+            .on("mousemove", function (d) {
+                // clog(this, event == d3.event);
+                // clog(event.clientY + 'px', event.clientX + 'px');
+                $("#rname").css({
+                    top: event.clientY - 30 + 'px',
+                    left: event.clientX + 'px'
+                }).text(d.properties[rgeo.prop_name]);
+            })
+            .append("title")
+            .text(d => d.properties[rgeo.prop_name]);
 
-            svg.selectAll("text")
-                .classed("selected", d2 => d == d2);
-        });
-    // .on("mouseenter", () => clog("enter"))
-    // .on("mouseleave", () => clog("leave"));
-    // .on("mouseover", () => clog("over"))
-    // .on("mouseout", () => clog("out"));
+        svg.selectAll("text")
+            .data(json.features)
+            .enter().append("text")
+            .attr("text-anchor", "middle")
+            .attr("transform", function (d) { return "translate(" + path.centroid(d) + ")"; })
+            .attr("class", "region-label")
+            .text(d => d.properties[rgeo.prop_name]);
+    });
 
-    svg.selectAll("text")
-        .data(json.features)
-        .enter().append("text")
-        .attr("text-anchor", "middle")
-        .attr("transform", function (d) { return "translate(" + path.centroid(d) + ")"; })
-        .attr("class", "region-label")
-        .text(d => d.properties[rgeo.prop_name]);
-});
+    function reset() {
+        svg.remove();
+        $("#rname").hide();
+    }
+}
